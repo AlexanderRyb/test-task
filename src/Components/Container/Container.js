835aa6c58f8c1f7603a46cf9c5a80544cfc7b5e7
Alexander <AlexanderRy@protonmail.com>
2022-03-31 23:04:06 +0300

    reworking. starting files

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -2,8 +2,22 @@ import React from 'react'
 import "./styles.css";
 
 
-export default function () {
+export default function Container () {
+  const getData = async()=>{
+
+    const res = await fetch("http://localhost:3001/products")
+    const data = await res.json()
+    console.log(data)
+    return data
+    
+  }
+
   return (
-    <div className="main-container"></div>
+   <div>
+     <h2>test</h2>
+     <button onClick={getData}>get data</button>
+   </div>
   )
 }
+
+  
